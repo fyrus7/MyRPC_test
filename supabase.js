5618b5ff183ecc2925411c93c145b38577c4411a
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2025-08-09 16:54:29 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 // ⚠️ Tukar URL & anon key ikut project Supabase kau
-const SUPABASE_URL = 'https://xxxxxxxxxxxx.supabase.co';
-const SUPABASE_KEY = 'YOUR_PUBLIC_ANON_KEY';
+const SUPABASE_URL = 'https://sfscvzzymtblukdupiyn.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmc2N2enp5bXRibHVrZHVwaXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3Mjg2NDAsImV4cCI6MjA3MDMwNDY0MH0.ELGQZckmEnD8QBC4b5rUl9f9l_QKiOykxwQ7DPGOy08';
 
 const { createClient } = supabase;
 window.supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
